1805ac0da76d0f1576cc5d2e08e54d4a10efebb0
faizan-abbasi <faizanabbasi.se@gmail.com>
2024-12-07 16:01:39 +0500

    Fizz Buzz

--- Easy.js
+++ Easy.js
@@ -1,5 +1,22 @@
+// Check for Empty Object
 const obj = { x: 5, y: 42 };
 const empObj = {};
 if (Object.keys(empObj).length === 0) {
   console.log(true);
 }
+
+// Fizz Buzz
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("Fizz Buzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(100);
